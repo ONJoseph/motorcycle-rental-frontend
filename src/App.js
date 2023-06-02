f678fea20d51cd7aca9eb3fd6a5c7fdab21ede36
diego <dgcuenca@gmail.com>
2023-06-02 09:31:12 -0500

    Set up route for log in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/Navbar';
+
+import Login from './pages/Login';
+import Registration from './pages/Registration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/register" element={<Registration />} /> */}
+        </Routes>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
