e0e3fe56901749d86a40032cfbe5d3f4bde42d55
diego <dgcuenca@gmail.com>
2023-06-05 14:34:36 -0500

    Style mobile view

--- src/components/VehicleSlider.jsx
+++ src/components/VehicleSlider.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  CarouselProvider, Slider, Slide, ButtonBack, ButtonNext,
+} from 'pure-react-carousel';
 import { useGetVehiclesQuery } from '../redux/vehicleSlice';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import VehicleCard from './VehicleCard';
+import styles from '../styles/Main.module.css';
 
 const VehicleSlider = () => {
   const {
@@ -10,18 +17,45 @@ const VehicleSlider = () => {
     error,
   } = useGetVehiclesQuery();
 
-  let content;
+  let cars;
   if (isLoading) {
-    content = <p>Loading...</p>;
+    cars = <p>Loading...</p>;
   } else if (isSuccess) {
-    content = JSON.stringify(vehicles);
+    cars = vehicles;
   } else if (isError) {
-    content = <p>{error}</p>;
+    cars = <p>{error}</p>;
   }
+
   return (
-    <>
-      {content}
-    </>
+    <div className={styles.productSlider}>
+      {cars && cars.length > 0 ? (
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          totalSlides={cars.length}
+          visibleSlides
+        >
+          <ButtonBack className={styles.btnPrev}>Back</ButtonBack>
+          <Slider>
+            {cars.map((car) => (
+              <Slide index={car.id - 1} key={car.id}>
+                <Link to={`/cars/${car.id}`}>
+                  <VehicleCard
+                    brand={car.brand}
+                    picture={car.picture}
+                    model={car.model}
+                    description={car.description}
+                  />
+                </Link>
+              </Slide>
+            ))}
+          </Slider>
+          <ButtonNext className={styles.btnNext}>Next</ButtonNext>
+        </CarouselProvider>
+      ) : (
+        <h2>{cars}</h2>
+      )}
+    </div>
   );
 };
 
